a40b54d75623f07af32b9c21c9dd7037d9138ed3
cactusfarmer <cactusplant@tutanota.com>
2022-06-23 20:12:07 +0100

    Added a style components to the project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,20 @@
 import './App.css';
+import styled from 'styled-components';
 import Wall from '../Wall/Wall';
 import data from '../../data/boards.json';
 import { WallData } from '../../types/wall_data';
 
+const Wrapper = styled.div`
+background: gray`;
+
 const { boards }: WallData = data;
 
 function App() {
-  return (<Wall data={boards} />);
+  return (
+    <Wrapper>
+      <Wall data={boards} />
+    </Wrapper>
+  );
 }
 
 export default App;
