725abf5270bdd903777d08ec2bacd0026c1ae053
cactusfarmer <cactusplant@tutanota.com>
2022-06-23 17:51:34 +0100

    Added eslint and ran a fix on a tsx file

--- src/componenents/Board/Board.tsx
+++ src/componenents/Board/Board.tsx
@@ -1,21 +1,25 @@
-import { BoardData } from "../../types/board_data"
-import Column from "../Column/Column"
+import { BoardData } from '../../types/board_data';
+import Column from '../Column/Column';
 
 type Props = {
     data: BoardData
 }
 
-const Board = ({ data }: Props): any => (
+function Board({ data }: Props): any {
+  return (
     <div className="board">
-        <h2>{data.name}</h2>
-        <div>
-            <ul className="columns">{
+      <h2>{data.name}</h2>
+      <div>
+        <ul className="columns">
+          {
                 data.columns.map((column) => (
-                    <Column data={column}></Column>
-                ))}
-            </ul>
-        </div>
+                  <Column data={column} />
+                ))
+}
+        </ul>
+      </div>
     </div>
-)
+  );
+}
 
-export default Board
+export default Board;
