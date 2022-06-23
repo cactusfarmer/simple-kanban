04c83747abce625828a0f05d347b502e5a2f6474
cactusfarmer <cactusplant@tutanota.com>
2022-06-23 17:35:26 +0100

    tidying up

--- src/componenents/App/App.tsx
+++ src/componenents/App/App.tsx
@@ -1,31 +1,12 @@
 import './App.css';
 import Wall from '../Wall/Wall';
-import data from '../../data/boards.json' 
+import data from '../../data/boards.json'
 import { WallData } from '../../types/wall_data';
 
-const wallData: WallData = data
-/* const wallData: WallData = {
-  "boards": [
-      {
-          "name": "Building work",
-          "columns": [
-              {
-                  "name": "To do",
-                  "cards": [
-                      {
-                          "name": "Chimney",
-                          "owner": "Matt",
-                          "info": "1 am monday please"
-                      }
-                  ]
-              }
-          ]
-      }
-  ]
-}*/
+const { boards }: WallData = data
 
 function App() {
-    return (<Wall data={data.boards}></Wall>);
+    return (<Wall data={boards}></Wall>);
 }
 
 export default App;
