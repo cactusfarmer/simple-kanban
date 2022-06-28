7045e64ef11e49c481ad38d5b71bdbcbd1f7f86a
cactusfarmer <cactusplant@tutanota.com>
2022-06-28 09:33:16 +0100

    removed an old file not being used

--- src/types/identifiable.tsx
+++
@@ -1,5 +0,0 @@
-import { BoardData } from './board_data';
-import { CardData } from './card_data';
-import { ColumnData } from './column_data';
-
-export type Identifyable = BoardData[] | ColumnData[] | CardData[];
