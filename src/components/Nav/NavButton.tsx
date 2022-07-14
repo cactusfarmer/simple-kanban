47be83430df5e579486bb79b5436af70090df46f
cactusfarmer <cactusplant@tutanota.com>
2022-07-14 15:40:42 +0100

    tidying up

--- src/components/Nav/NavButton.tsx
+++ src/components/Nav/NavButton.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const NavButton = styled.button.attrs((props: { selected: Boolean }) => props)`
-display:inline-block;
+cursor: pointer;
+display:block;
 padding:0.3em 1.2em;
 margin:0 0.3em 0.3em 0;
 border-radius:2em;
